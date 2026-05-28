5e10073ea1b0586ccd0f5bcc2b8c65a9d5a2e4c8
David Andersson <lejonmanen@gmail.com>
2026-05-28 11:58:00 +0200

    Test agile helper: sprint end

--- tests/agile-helper.spec.js
+++ tests/agile-helper.spec.js
@@ -34,4 +34,40 @@ test.describe('Agile helper', () => {
 		await expect(heading).toBeVisible()
 	})
 
+
+	/*
+	1c Gör ett test för scenariot:
+	Klicka på knappen med texten "Sista"
+	Klicka på knappen "Sprint review"
+	Kontrollera att en rubrik med texten "Sprint review" är synlig på webbsidan.
+	Klicka på knappen "Dags för retrospective"
+	Klicka på knappen "Sprint retrospective"
+	Kontrollera att rubriken "Sprint retrospective" visas.
+	*/
+	test('som en användare, vill jag läsa om hur jag avslutar sprinten, så att jag håller scrum-möten på rätt sätt', async ({ page }) => {
+		const animationTimeout = { timeout: 2000 }
+
+		await page.getByTestId('btn-last').click()
+		await page.getByRole('button', { name: 'Sprint review' }).click()
+		const reviewHeading = page.getByRole('heading', { name: 'Sprint review' })
+		expect(reviewHeading).toBeVisible(animationTimeout)
+
+		// Stänga dialogen
+		await page.getByRole('button', { name: /Dags för retrospective/ }).click(animationTimeout)
+		await page.getByRole('button', { name: 'Sprint retrospective' }).click(animationTimeout)
+		const retroHeading = page.getByRole('heading', { name: 'Sprint retrospective' })
+		expect(retroHeading).toBeVisible()
+	})
 })
+
+/*
+Effektivare kod genom att lyfta ut locators m.m. till ett objekt
+const pageObject = {
+	btnLast: page.getByTestId('btn-last'),
+	closeReviewDialog: async () => await page.getByRole('button', { name: /Dags för retrospective/ }).click(animationTimeout)
+}
+
+Använd så här:
+await pageObject.btnLast.click()
+await pageObject.closeReviewDialog()
+*/
